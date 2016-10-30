5f40aa4737bb876818264beb5d18b70b68d023e2
Ivan <kovic.ivan@gmail.com>
2016-10-30 21:18:00 +0100

    Adding more UI stuff

--- src/client/components/tabsLayout/tabsLayout.js
+++ src/client/components/tabsLayout/tabsLayout.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Layout, Header, HeaderRow, HeaderTabs, Tab, Drawer, Content } from 'react-mdl';
+import {Layout, Header, HeaderRow, HeaderTabs, Tab, Drawer, Content} from 'react-mdl';
 
 export default class TabsLayout extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {activeTab: 2};
+        this.state = {activeTab: 0};
     }
 
     render() {
@@ -13,18 +13,21 @@ export default class TabsLayout extends React.Component {
             <div style={{height: '300px', position: 'relative'}}>
                 <Layout fixedHeader>
                     <Header>
-                        <HeaderRow title="Title"/>
+                        <HeaderRow title="Distribucija troškova po stavci proračuna"/>
                         <HeaderTabs ripple activeTab={this.state.activeTab}
                                     onChange={(tabId) => this.setState({activeTab: tabId})}>
-                            <Tab>Tab1</Tab>
-                            <Tab>Tab2</Tab>
-                            <Tab>Tab3</Tab>
-                            <Tab>Tab4</Tab>
-                            <Tab>Tab5</Tab>
-                            <Tab>Tab6</Tab>
+                            <Tab>Narudžbenice</Tab>
+                            <Tab>Proračun</Tab>
+                            <Tab>Dobavljači</Tab>
+                            <Tab>Troškovi</Tab>
                         </HeaderTabs>
                     </Header>
-                    <Drawer title="Title"/>
+                    <Drawer title="Postavke">
+                        <ul>
+                            <li>Korisnici</li>
+                            <li>Organizacija</li>
+                        </ul>
+                    </Drawer>
                     <Content>
                         <div className="page-content">Content for the tab: {this.state.activeTab}</div>
                     </Content>
