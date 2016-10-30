950fdfff4f8d80a7f7617bcc99e6b78447a12ad8
Ivan <kovic.ivan@gmail.com>
2016-10-30 21:05:37 +0100

    Removing unused code

--- src/client/components/login/loginRequired.js
+++ src/client/components/login/loginRequired.js
@@ -3,28 +3,14 @@ import {withRouter} from 'react-router';
 import {connect} from 'react-redux';
 
 class UndecoratedLoginRequired extends Component {
-    constructor() {
-        super();
-        this._onChange = this._onChange.bind(this);
-    }
-
-    _onChange() {
-        var loggedIn = !this.props.loggedIn;
-        if (!loggedIn) {
-            this.props.router.push('/login');
-        }
-    }
 
     componentWillMount() {
-        //sessionStore.addChangeListener(this._onChange);
-
         if (!this.props.loggedIn) {
             this.props.router.push('/login');
         }
     }
 
     render() {
-        console.log(this.props);
         return (
             this.props.children
         );
@@ -36,7 +22,8 @@ UndecoratedLoginRequired.propTypes = {
         push: PropTypes.func.isRequired
     }).isRequired,
     location: PropTypes.object,
-    children: PropTypes.any
+    children: PropTypes.any,
+    loggedIn: PropTypes.bool
 };
 
 function mapStateToProps(state) {
