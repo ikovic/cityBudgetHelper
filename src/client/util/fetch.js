00168cebd264919f17d0102f833bc6a7786c79b1
Ivan Kovic <kovic.ivan@gmail.com>
2016-11-04 16:09:38 +0100

    Adding auth header to fetch

--- src/client/util/fetch.js
+++ src/client/util/fetch.js
@@ -1,17 +1,31 @@
 import {fetchUrl} from 'fetch';
+import store from '../redux/store';
+
+
+function getHeaders() {
+    let headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    };
+    let token = store.getState().session.token;
+    if (token) {
+        headers['Authorization'] = 'JWT ' + token;
+    }
+}
 
 export function get(url, callback) {
-    fetchUrl(url, callback);
+    fetchUrl(url, {
+            method: 'GET',
+            headers: getHeaders()
+        },
+        callback);
 }
 
 export function post(url, body, callback) {
     fetchUrl(url, {
             method: 'POST',
             payload: JSON.stringify(body),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
+            headers: getHeaders()
         },
         callback);
 }
