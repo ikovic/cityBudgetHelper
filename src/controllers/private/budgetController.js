4d7ad3cfa21eab5d8608058c2fec10fe12375524
igor.bebic@gmail.com <igor.bebic@gmail.com>
2016-11-04 14:13:49 +0100

    Added listing budgets by id

--- src/controllers/private/budgetController.js
+++ src/controllers/private/budgetController.js
@@ -16,6 +16,18 @@ router.route('/budgets')
                 res.json(error);
             });
     });
+
+    router.route('/budgets/:budgets_id')
+        .get(function (req, res) {
+            models.Budget.findById(req.params.budgets_id)
+                .then(function (value) {
+                    res.json(value);
+                })
+                .catch(function (error) {
+                    console.log('Error with GET budget:', error);
+                    res.json(error);
+                });
+        });
 // budget / id stuff
 // add filtering on /budgets route by organization. see 'rest api filtering express'
 // Postman - set header Authorization header / jwt from tokena
