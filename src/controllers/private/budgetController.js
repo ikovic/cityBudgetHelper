f99eae80c70386855ca19db4b3ae03863b631eb8
igor.bebic@gmail.com <igor.bebic@gmail.com>
2016-11-04 14:42:34 +0100

    Added budget filtering by OrganizationId

--- src/controllers/private/budgetController.js
+++ src/controllers/private/budgetController.js
@@ -7,6 +7,7 @@ var express = require('express'),
  */
 router.route('/budgets')
     .get(function (req, res) {
+      if (!req.query.org_id){
         models.Budget.findAll()
             .then(function (value) {
                 res.json(value);
@@ -15,6 +16,16 @@ router.route('/budgets')
                 console.log('Error with GET budget:', error);
                 res.json(error);
             });
+          } else {
+            models.Budget.findAll({ where: {OrganizationId: req.query.org_id}})
+                .then(function (value) {
+                    res.json(value);
+                })
+                .catch(function (error) {
+                    console.log('Error with GET budget:', error);
+                    res.json(error);
+                });
+          }
     });
 
     router.route('/budgets/:budgets_id')
@@ -28,7 +39,6 @@ router.route('/budgets')
                     res.json(error);
                 });
         });
-// budget / id stuff
 // add filtering on /budgets route by organization. see 'rest api filtering express'
 // Postman - set header Authorization header / jwt from tokena
 module.exports = router;
