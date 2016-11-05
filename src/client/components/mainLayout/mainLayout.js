873983e57bf13da71a8e9fe09f46cf6f4e30bd28
Ivan <kovic.ivan@gmail.com>
2016-11-05 17:34:05 +0100

    Added polyfill for MDL dialog. Todo: redux modals

--- src/client/components/mainLayout/mainLayout.js
+++ src/client/components/mainLayout/mainLayout.js
@@ -4,6 +4,8 @@ import Sidebar from './sidebar/sidebar';
 import Header from './header/header';
 import Budget from './budget/budget';
 import Orders from './orders/orders';
+import BudgetDialog from '../dialog/budgetDialog';
+import dialogPolyfill from 'dialog-polyfill';
 
 export default class MainLayout extends React.Component {
 
@@ -34,14 +36,21 @@ export default class MainLayout extends React.Component {
         });
     }
 
+    // TODO http://stackoverflow.com/questions/35623656/how-can-i-display-a-modal-dialog-in-redux-that-performs-asynchronous-actions/35641680#35641680
+    componentDidMount() {
+        // for the dialog example, we have to register the dialogs window
+        const dialogs = document.querySelector("dialog"); dialogs && dialogPolyfill.registerDialog(dialogs);
+    }
+
     render() {
         return (
-            <div id="tabsLayout" >
-                <Layout fixedHeader >
-                    <Header activeTab={this.state.activeTab} changeTab={this.handleTabChange} />
+            <div id="tabsLayout">
+                <BudgetDialog/>
+                <Layout fixedHeader>
+                    <Header activeTab={this.state.activeTab} changeTab={this.handleTabChange}/>
                     <Sidebar />
-                    <Content id="appContent" >
-                        <div className="page-content" >{this.getDisplayElement(this.state.activeTab)}</div>
+                    <Content id="appContent">
+                        <div className="page-content">{this.getDisplayElement(this.state.activeTab)}</div>
                     </Content>
                 </Layout>
             </div>
