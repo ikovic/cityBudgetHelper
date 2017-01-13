9b001e9dc99a264bc0b1b37390c2360143a50dd3
Ivan Kovic <kovic.ivan@gmail.com>
2017-01-13 08:27:15 +0100

    editor config stolen from airbnb

--- src/controllers/public/authController.js
+++ src/controllers/public/authController.js
@@ -7,35 +7,35 @@ const config = require('../../config/config');
 const models = require('../../models');
 
 router.route('/token')
-    .post(function (req, res) {
-        const {email, password} = req.body;
-        
-        if (!email || !password) {
-            return res.sendStatus(400);
-        }
+  .post(function (req, res) {
+    const {email, password} = req.body;
 
-        authenticate(email, password)
-            .then(authData => res.json(authData))
-            .catch(() => res.sendStatus(401));
-    });
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    authenticate(email, password)
+      .then(authData => res.json(authData))
+      .catch(() => res.sendStatus(401));
+  });
 
 function authenticate(email, password) {
-    var existingUser;
-    return getUser(email)
-        .then(user => {
-            existingUser = user;
-            return user.validatePassword(password);
-        })
-        .then(() => {
-            const payload = {id: existingUser.id};
-            const secret = config.jwt.secret;
-            const token = jwt.encode(payload, secret);
-            return {token, user: existingUser.toJson()}
-        });
+  var existingUser;
+  return getUser(email)
+    .then(user => {
+      existingUser = user;
+      return user.validatePassword(password);
+    })
+    .then(() => {
+      const payload = {id: existingUser.id};
+      const secret = config.jwt.secret;
+      const token = jwt.encode(payload, secret);
+      return {token, user: existingUser.toJson()}
+    });
 }
 
 function getUser(email) {
-    return models.User.findOne({where: {email}});
+  return models.User.findOne({where: {email}});
 }
 
 module.exports = router;
