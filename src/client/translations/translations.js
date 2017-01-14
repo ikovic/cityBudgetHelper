fbdbd8e82a641c909578114feb74d308d1b6e710
Ivan <kovic.ivan@gmail.com>
2017-01-14 13:16:15 +0100

    Budget Item translated

--- src/client/translations/translations.js
+++ src/client/translations/translations.js
@@ -6,7 +6,14 @@ const resources = {
       [keys.LOGIN.TITLE]: "Prijava",
       [keys.LOGIN.USERNAME]: "Email",
       [keys.LOGIN.PASSWORD]: "Lozinka",
-      [keys.LOGIN.CONFIRM]: "Kreni"
+      [keys.LOGIN.CONFIRM]: "Kreni",
+      [keys.BUDGET_ITEM.CARD_TITLE]: "Stavka proračuna",
+      [keys.BUDGET_ITEM.POSITION]: "Pozicija",
+      [keys.BUDGET_ITEM.DESCRIPTION]: "Opis",
+      [keys.BUDGET_ITEM.AMOUNT]: "Iznos",
+      [keys.BUDGET_ITEM.AMOUNT_ERROR]: "Unesite ispravan iznos",
+      [keys.BUDGET_ITEM.CONFIRM]: "OK",
+      [keys.BUDGET_ITEM.CANCEL]: "Odustani"
     }
   },
   "en": {
@@ -14,7 +21,14 @@ const resources = {
       [keys.LOGIN.TITLE]: "Log In",
       [keys.LOGIN.USERNAME]: "Email",
       [keys.LOGIN.PASSWORD]: "Password",
-      [keys.LOGIN.CONFIRM]: "Go"
+      [keys.LOGIN.CONFIRM]: "Go",
+      [keys.BUDGET_ITEM.CARD_TITLE]: "Budget Item",
+      [keys.BUDGET_ITEM.POSITION]: "Position",
+      [keys.BUDGET_ITEM.DESCRIPTION]: "Description",
+      [keys.BUDGET_ITEM.AMOUNT]: "Amount",
+      [keys.BUDGET_ITEM.AMOUNT_ERROR]: "Enter a valid amount",
+      [keys.BUDGET_ITEM.CONFIRM]: "OK",
+      [keys.BUDGET_ITEM.CANCEL]: "Cancel"
     }
   }
 };
