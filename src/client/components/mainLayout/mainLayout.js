a3b49ea0107004d18bca234383d89da0174eec1f
Ivan <kovic.ivan@gmail.com>
2017-01-14 14:15:15 +0100

    Whitespace fix in mainLayout

--- src/client/components/mainLayout/mainLayout.js
+++ src/client/components/mainLayout/mainLayout.js
@@ -12,73 +12,73 @@ import dialogPolyfill from 'dialog-polyfill';
 
 class MainLayout extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleTabChange = this.handleTabChange.bind(this);
+    this.handleTabChange = this.handleTabChange.bind(this);
 
-        this.state = {activeTab: 1};
+    this.state = {activeTab: 1};
+  }
+
+  getDisplayElement(tabId) {
+    switch (tabId) {
+      case 0:
+        return <Orders/>;
+      case 1:
+        return <Budget/>;
+      case 2:
+        return tabId;
+      case 3:
+        return tabId;
     }
+  }
 
-    getDisplayElement(tabId) {
-        switch (tabId) {
-            case 0:
-                return <Orders/>;
-            case 1:
-                return <Budget/>;
-            case 2:
-                return tabId;
-            case 3:
-                return tabId;
+  handleTabChange(tabId) {
+    this.setState({
+      activeTab: tabId
+    });
+  }
+
+  // TODO http://stackoverflow.com/questions/35623656/how-can-i-display-a-modal-dialog-in-redux-that-performs-asynchronous-actions/35641680#35641680
+  // <BudgetDialog/>
+  componentDidMount() {
+    // for the dialog example, we have to register the dialogs window
+    const dialogs = document.querySelector("dialog");
+    dialogs && dialogPolyfill.registerDialog(dialogs);
+
+    get('http://localhost:3000/api/organizations/' + this.props.user.orgId,
+      null,
+      (error, meta, body) => {
+        if (!error && meta.status == 200) {
+          let org = JSON.parse(body.toString());
+          this.props.dispatch(actions.loadOrganization(org));
         }
-    }
+      });
+  }
 
-    handleTabChange(tabId) {
-        this.setState({
-            activeTab: tabId
-        });
-    }
-
-    // TODO http://stackoverflow.com/questions/35623656/how-can-i-display-a-modal-dialog-in-redux-that-performs-asynchronous-actions/35641680#35641680
-    // <BudgetDialog/>
-    componentDidMount() {
-        // for the dialog example, we have to register the dialogs window
-        const dialogs = document.querySelector("dialog");
-        dialogs && dialogPolyfill.registerDialog(dialogs);
-
-        get('http://localhost:3000/api/organizations/' + this.props.user.orgId,
-            null,
-            (error, meta, body) => {
-                if (!error && meta.status == 200) {
-                    let org = JSON.parse(body.toString());
-                    this.props.dispatch(actions.loadOrganization(org));
-                }
-            });
-    }
-
-    render() {
-        return (
-            <div id="tabsLayout" >
-                <Layout fixedHeader >
-                    <Header activeTab={this.state.activeTab} changeTab={this.handleTabChange} />
-                    <Sidebar />
-                    <Content id="appContent" >
-                        <div className="page-content" >{this.getDisplayElement(this.state.activeTab)}</div>
-                    </Content>
-                </Layout>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="tabsLayout">
+        <Layout fixedHeader>
+          <Header activeTab={this.state.activeTab} changeTab={this.handleTabChange}/>
+          <Sidebar />
+          <Content id="appContent">
+            <div className="page-content">{this.getDisplayElement(this.state.activeTab)}</div>
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        user: state.session.user
-    };
+  return {
+    user: state.session.user
+  };
 }
 
 MainLayout.propTypes = {
-    user: PropTypes.object.isRequired
+  user: PropTypes.object.isRequired
 };
 
 export default connect(mapStateToProps)(MainLayout);
