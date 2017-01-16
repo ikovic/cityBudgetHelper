281bffd6627b2d802336097367c7e797e426e457
Ivan Kovic <kovic.ivan@gmail.com>
2017-01-16 07:35:11 +0100

    In progress: translation component

---
+++ src/client/components/util/translation.js
@@ -0,0 +1,15 @@
+import React, {Component, PropTypes} from 'react';
+
+
+export default class Translation {
+
+
+
+  render() {
+
+  }
+}
+
+Translation.propTypes = {
+  translationKey: PropTypes.string.isRequired
+};
--- src/client/redux/reducers/index.js
+++ src/client/redux/reducers/index.js
@@ -3,12 +3,14 @@ import sessionReducer from './sessionReducer';
 import organizationReducer from './organizationReducer';
 import budgetReducer from './budgetReducer';
 import budgetItemsReducer from './budgetItemsReducer';
+import translationReducer from './translationReducer';
 
 const rootReducer = combineReducers({
-    session: sessionReducer,
-    organization: organizationReducer,
-    budget: budgetReducer,
-    budgetItems: budgetItemsReducer
+  session: sessionReducer,
+  organization: organizationReducer,
+  budget: budgetReducer,
+  budgetItems: budgetItemsReducer,
+  translation: translationReducer
 });
 
 export default rootReducer;
