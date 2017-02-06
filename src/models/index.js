c6b456d4d143a1d731792c69f2614645e9124c6a
Ivan Kovic <kovic.ivan@gmail.com>
2017-02-06 10:30:09 +0100

    Formatting

--- src/models/index.js
+++ src/models/index.js
@@ -8,19 +8,19 @@ const sequelize = new Sequelize(config.db.database, config.db.username, config.d
 const db = {};
 
 fs
-    .readdirSync(__dirname)
-    .filter(function (file) {
-        return (file.indexOf(".js") !== 0) && (file !== "index.js");
-    })
-    .forEach(function (file) {
-        let model = sequelize.import(path.join(__dirname, file));
-        db[model.name] = model;
-    });
+  .readdirSync(__dirname)
+  .filter(function (file) {
+    return (file.indexOf(".js") !== 0) && (file !== "index.js");
+  })
+  .forEach(function (file) {
+    let model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach(function (modelName) {
-    if (db[modelName].hasOwnProperty('associate')) {
-        db[modelName].associate(db);
-    }
+  if (db[modelName].hasOwnProperty('associate')) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
